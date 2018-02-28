467a46dbae72c9b5008e4d0e42e5ac299840a6e9
lester <yuenlee.hk@gmail.com>
2018-02-28 18:17:32 +0800

    s

--- src/test/java/Utils/OpenCSV.java
+++ src/test/java/Utils/OpenCSV.java
@@ -2,11 +2,16 @@ package Utils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 
 import AppiumSupport.Config;
+import Test.TestSuiteContainer;
 import au.com.bytecode.opencsv.CSVReader;
 
 public class OpenCSV {
@@ -18,42 +23,55 @@ public class OpenCSV {
 	
     public static void main(String[] args) throws Exception {
         OpenCSV oc = new OpenCSV();
-        oc.CSVReadAll();
+        
 
       
     }
 
-    public String dir = System.getProperty("user.dir") + "/conf";
+    public String yamlDir = System.getProperty("user.dir") + "/Yaml/";
 
-    public void CSVReadAll() throws Exception {
-        File csv = new File(dir , "TestCaseList.csv");
+    public ArrayList<TestSuiteContainer> CreateAllTestSuiteContainer(String csvFile)  {
+        
+    	ArrayList<TestSuiteContainer> testcases = new ArrayList<TestSuiteContainer>();
+    	
+//    	ArrayList<File> files= new ArrayList<File>();
+//    	for (String fileName:fileNameList) {
+//    		files.add(new File(yamlDir+fileName));
+//    	}
+    	
+    	File file= new File(System.getProperty("user.dir")+"/conf/TestSuiteList.csv");
+    	
+    		try {
+    			CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream(file), "UTF-8"), ',');
+				List<String[]> list;
+				list = reader.readAll();
+				
+				String[] s;
+				for (int i =0; i<list.size(); i++) {
+					s= list.get(i);
+					int size = s.length;
+					List<String> yamlpath=new ArrayList<String>();;
+					
+					for (int y=1; y<size; y++) {
+						yamlpath.add(s[y]);
+					}
+					
+					testcases.add(new TestSuiteContainer(s[0], yamlpath));
+				}
+			
+			}catch (UnsupportedEncodingException | FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
-        CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream(csv), "UTF-8"), ',');
-        List<String[]> list = reader.readAll(); //此时读取的已经是第二行了
-        
-        
-        for (String[] s : list) {
-            System.out.print(s + ",");
             
-        }
+    
+    	return testcases;
 
     }
     
-    public File[] getFileList(){
-		File dir = new File(getTargetCsvFilePath());
-		File[] files = dir.listFiles(new FilenameFilter() {
-		    public boolean accept(File dir, String name) {
-				return name.toLowerCase().endsWith(fileExtension);
-		    }
-		});
-		return files;
-	}
-    
-	public String getTargetCsvFilePath(){
-		
-		Config instance=Config.getInstance();
-		return instance.getProperty("resource.csv");
-		
-	}
 
 }
